97f66fe93c95a472fe486231a297e59b5edea6eb
Shehan Chrishmika <shehanchrishmika123@gmail.com>
2025-09-10 08:48:19 +0530

    Enhance carbig.m with visualization and correlation

--- 2025-09-02/carbig.m
+++ 2025-09-02/carbig.m
@@ -32,3 +32,16 @@ X_norm = normalize(X,'range');
 
 %Standardization
 X_std=zscore(X);
+
+%visualization
+subplot(1,2,1);histogram(X(:,1));title('Original Hoursepower');
+subplot(1,2,2);histogram(X_std(:,1));title('Standardised Horsepower');
+
+tbl_clean.Cylinders = categorical(tbl_clean.Cylinders);
+cyl_encoded = dummyvar(tbl_clean.Cylinders);
+disp(cyl_encoded(1:5,:))
+
+corrVals = corr(tbl_clean{:,{'Horsepower','Weight','Acceleration'}},tbl_clean.MPG,'Rows','complete');
+
+disp('Correlation of feature with MPG:');
+disp(array2table(corrVals,'VariableNames',{'Correlation'},'RowNames',{'Horsepower','Weight','Acceleration'}));
